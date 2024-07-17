b794fe5602d6650846aa7aa5a77098b219dc24a9
Noble Benjamin <nobleben2008@gmail.com>
2024-07-17 19:08:29 +0100

    Made function for Click handler for the stars

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -6,7 +6,9 @@ const StarRating = ({ maxRating }) => {
   return (
     <div className="star-container">
       {Array.from({ length: maxRating }, (_, i) => {
-        return <Star key={i} full={rating <= i + 1} />;
+        return <Star key={i} full={rating >= i + 1} onRate={() => {
+            setRating(i+1)
+        }}/>;
       })}
     </div>
   );
