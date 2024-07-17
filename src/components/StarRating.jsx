0b42c74563bb55a5c7012a453aa98ec6eff526a1
Noble Benjamin <nobleben2008@gmail.com>
2024-07-17 19:21:23 +0100

    Added the Function for temporary hoverIn rating

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -14,6 +14,7 @@ const StarRating = ({ maxRating }) => {
             onRate={() => {
               setRating(i + 1);
             }}
+            onHoverIn={setHoverRating(i + 1)}
           />
         );
       })}
