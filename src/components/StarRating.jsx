0796f62ed8c9e7a5f5e1586b9248dbe6e5a75940
Noble Benjamin <nobleben2008@gmail.com>
2024-07-17 19:01:46 +0100

    Added a div to make stars have a flex display

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import Star from "./Star";
 
 const StarRating = ({ maxRating }) => {
-    const [rating, setRating] = useState(0)
-  return Array.from({ length: maxRating }, (_, i) => {
-    return (
-      <Star key={i} full={rating <= i + 1}/>
-    );
-  });
+  const [rating, setRating] = useState(0);
+  return (
+    <div className="star-container">
+      {Array.from({ length: maxRating }, (_, i) => {
+        return <Star key={i} full={rating <= i + 1} />;
+      })}
+    </div>
+  );
 };
 
 export default StarRating;
